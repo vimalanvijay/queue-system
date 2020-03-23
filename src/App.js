2d0ca6d148660ca4e7a777d5bf37a1887afc9b9d
vimalanvijay <@/tXZ4Z`vP?p(8[>
2020-03-23 12:00:44 +0800

    initialisation

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from "react"
+import Header from "./components/Header"
+import MainContent from "./components/MainContent"
+
+function App(){
+    return (
+        <div>
+            <Header />
+            <MainContent />
+        </div>
+    )
+}
+
+export default App
---
+++ src/components/MainContent.js
@@ -0,0 +1,9 @@
+import React from "react"
+
+function MainContent(){
+    return(
+        <main>Hlelo</main>
+    )
+}
+
+export default MainContent
