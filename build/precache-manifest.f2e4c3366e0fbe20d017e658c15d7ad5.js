8b5fb8f8ef0adf5ad0bfc1ef957d0af302cff357
vimalanvijay <@/tXZ4Z`vP?p(8[>
2020-03-23 18:49:51 +0800

    build1.0

---
+++ build/precache-manifest.f2e4c3366e0fbe20d017e658c15d7ad5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "30c74a6b75faa282e4b33b42b8e96ba9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d713e884d8ddf1568f6b",
+    "url": "/static/css/main.8cee47b8.chunk.css"
+  },
+  {
+    "revision": "b2980eacf02c645b76b1",
+    "url": "/static/js/2.40ad4c16.chunk.js"
+  },
+  {
+    "revision": "bad6e083d4d2a996fe60e513745a4089",
+    "url": "/static/js/2.40ad4c16.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d713e884d8ddf1568f6b",
+    "url": "/static/js/main.ed52084e.chunk.js"
+  },
+  {
+    "revision": "2a6b2f3549a5bac6e124",
+    "url": "/static/js/runtime-main.f1c084ba.js"
+  }
+]);
